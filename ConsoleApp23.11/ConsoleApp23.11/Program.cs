885f1a045abbbe96b27b998cbcdea151d8bcf7c7
plaskina46 <118421132+plaskina46@users.noreply.github.com>
2022-11-23 09:29:55 +0300

    Add files via upload

---
+++ ConsoleApp23.11/ConsoleApp23.11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace CsharpString
+{
+    class Test
+    {
+        /* public static void Main(string[] args)
+          {
+
+              string s1 = "Строка";
+              string s2 = "Строка";
+              int res = string.Compare(s1, s2);
+              Console.WriteLine(res);
+          }*/
+        /*  public static void Main(string[] args)
+          {
+
+              string str1 = "Ice cream";
+              string str2 = "Ice cream1";
+
+
+              bool result = String.Equals(str1, str2);
+
+              Console.WriteLine(result);
+
+              Console.ReadLine();
+          }*/
+        /*static void Main(string[] args)
+        {
+            string str1 = "cat";
+            string str2 = "apple";
+            int res = String.CompareOrdinal(str2, str1);
+
+            Console.WriteLine("Difference is {0}", res);
+        }*/
+     /*   static void Main()
+        {
+            
+            const string val = "13,34";
+            
+            if (val.IndexOf(",") != -1)
+            {
+                Console.WriteLine("The character , is present in the specified string");
+            }
+
+        }*/
+
+  
+
+    
+
+       
+     }
+}
+
